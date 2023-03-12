c324707fa9405c8d6d3907857ee785cb8dd1661e
Joaquin <joaquinsepulveda@hotmail.com>
2023-03-13 00:04:56 +0100

    Fix missing refactored element

--- lib/warchest.rb
+++ lib/warchest.rb
@@ -15,6 +15,6 @@ puts "Enter second player name"
 second_player_name = gets.chomp
 player_1 = Player.new(first_player_name)
 player_2 = Player.new(second_player_name)
-game = WarchestLite::Game.new(player_1, player_2)
+game = Game.new(player_1, player_2)
 game.setup_game
 game.start_game
